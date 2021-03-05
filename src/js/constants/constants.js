504d74b9802060fe24d3a159b8baeb7b91a20aa7
notReally08 <dasha.pavlovna2014@yandex.ru>
2021-03-05 13:58:16 +0300

    extension: modify active elements to separate objects

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,54 +1,45 @@
-// array with dom elements
-
-// const test = [
-//     {
-//         elementSelector: ".input_sdnsd",
-//         event: "enter",
-//         handler: "test"
-//     },{
-//         elementSelector: ".input_sdnsd",
-//         event: "enter",
-//         handler: "test"
-//     }
-// ];
-
-const activeElements = [
-    {
-        elementSelector: ".btn-delete",
-        eventName: "click",
-        handler: "deleteTask"
-    },
-    {
-        elementSelector: ".footer-btn",
-        eventName: "click",
-        handler: "deleteAllTasks"
-    },
-    {
-        elementSelector: ".add-tasks__btn",
-        eventName: "click",
-        handler: "addTask"
-    },
-    {
-        elementSelector: ".add-tasks__input",
-        eventName: "enter",
-        handler: "addTask" 
-    },
-    {
-        elementSelector: ".task__checkbox",
-        eventName: "click",
-        handler: "changeStatus" 
-    },
-    {
-        elementSelector: ".task__text",
-        eventName: "enter",
-        handler: "editTextTask" 
-    },
-    {
-        elementSelector: ".task__text",
-        eventName: "change",
-        handler: "editTextTask" 
-    }
-]
+// export handlers.js
+const activeElements = {
+    staticElements: [
+        {
+            elementSelector: ".add-tasks__input",
+            eventName: "enter",
+            handler: "addTask" 
+        },
+        {
+            elementSelector: ".add-tasks__btn",
+            eventName: "click",
+            handler: "addTask"
+        },
+        {
+            elementSelector: ".footer-btn",
+            eventName: "click",
+            handler: "deleteAllTasks"
+        }
+    ],
+    dynamicElements: [
+        {
+            elementSelector: ".btn-delete",
+            eventName: "click",
+            handler: "deleteTask"
+        },
+        {
+            elementSelector: ".task__checkbox",
+            eventName: "click",
+            handler: "changeStatus" 
+        },
+        {
+            elementSelector: ".task__text",
+            eventName: "enter",
+            handler: "editTextTask" 
+        },
+        {
+            elementSelector: ".task__text",
+            eventName: "change",
+            handler: "editTextTask" 
+        }
+    ]
+}
 
 // 1) btn-delete --> click (удалить одну задачу);
 // 2) footer__btn --> click (удалить все задачи);
