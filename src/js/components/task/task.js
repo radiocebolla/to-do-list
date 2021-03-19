0bda22525d3240fc166223f8c9763e49337388f5
notReally08 <dasha.pavlovna2014@yandex.ru>
2021-03-19 20:49:54 +0300

    fix: add space between classes

--- src/js/components/task/task.js
+++ src/js/components/task/task.js
@@ -3,7 +3,7 @@ import { taskDeleteBtn } from "./../task/taskDeleteBtn";
 import { taskText } from "./../task/taskText";
 
 export const Task = (task) => {
-    return `<li class="list__item ${task.status ? "list__item_done" : ""}task" data-id="${task.id}">
+    return `<li class="list__item ${task.status ? "list__item_done" : ""} task" data-id="${task.id}">
                 ${label(task.status)}
                 ${taskText(task.text)}
                 ${taskDeleteBtn()}
