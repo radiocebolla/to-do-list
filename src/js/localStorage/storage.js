4100eaba21090475062cc5757574cb88818f877f
radiocebolla <71603171+radiocebolla@users.noreply.github.com>
2021-07-03 19:37:01 +0300

    Update storage.js

--- src/js/localStorage/storage.js
+++ src/js/localStorage/storage.js
@@ -1,20 +1,3 @@
-// const defaultTasks = [
-//     {
-//         id: 1,
-//         text: "Съесть сырную пиццу 2 раза",
-//         status: true
-//     },
-//     {
-//         id: 2,
-//         text: "Позлословить",
-//         status: false
-//     },
-//     {
-//         id: 3,
-//         text: "Глянуть ссылку",
-//         status: true
-//     }
-// ];
 
 export function localStorageInit() {
     if(!localStorage.getItem("taskListArray")) {
@@ -72,9 +55,7 @@ export function changeStatusInStorage(dataId) {
     localStorage.setItem("taskListArray", JSON.stringify(newTasksArray));
 }
 
-// положить в переменнную текущий список задач
-// создать новую переменную
-// положить туда новый массив
+
 export function deleteTaskFromStorage(dataId) {
     const currentTasks = JSON.parse(localStorage.getItem("taskListArray"));
     const newTasksArray = currentTasks.filter(function(task){
