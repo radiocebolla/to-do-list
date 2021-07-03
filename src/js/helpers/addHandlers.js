6e320dc3c62cbfe397b4e2d8ac00d8ff7a563a1e
radiocebolla <71603171+radiocebolla@users.noreply.github.com>
2021-07-03 19:34:19 +0300

    Update addHandlers.js

--- src/js/helpers/addHandlers.js
+++ src/js/helpers/addHandlers.js
@@ -1,15 +1,5 @@
 export function addHandlers(elements, removeListener) {
-    // {
-    //     elementSelector: ".btn-delete",
-    //     eventName: "click",
-    //     handler: "deleteTask"
-    // },
-    /*
-    1. Найти элемент в разметке
-    2. Навесить addEventListener на каждый
-    3. Написать условие, которое проверяет, надо ли removeEventListener
-    */
-    // attachEvent(textarea, 'blur', onblur);
+
     elements.forEach(function(element){
         const domElements = document.querySelectorAll(element.elementSelector);
         domElements.forEach(function(domElement){
@@ -20,9 +10,3 @@ export function addHandlers(elements, removeListener) {
     })
 }
 
-//if(element.elementSelector === ".footer__btn") { // disable link redirect
-// e.preventDefault();
-// }
-// if(e.type === "keydown" && e.keyCode === 13) {
-//     console.log("Enter!!!");
-// }
